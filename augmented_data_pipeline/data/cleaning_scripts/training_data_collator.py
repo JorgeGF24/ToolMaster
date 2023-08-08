ecd0d3486efb292ec646e9eccdd19d4b78dd9d11
j <jg2619@ic.ac.uk>
2023-08-08 21:05:57 +0100

    better training collator

--- augmented_data_pipeline/data/cleaning_scripts/training_data_collator.py
+++ augmented_data_pipeline/data/cleaning_scripts/training_data_collator.py
@@ -10,112 +10,109 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 cache_dir = "/vol/bitbucket/jg2619/augmenting_llms/augmented_data_pipeline/toolformer/cache"
 tokenizer = AutoTokenizer.from_pretrained("/vol/bitbucket/jg2619/models/tokenizer", truncate=True, max_length=270, cache_dir=cache_dir)
-tokenizer2 = AutoTokenizer.from_pretrained("gpt2", truncate=True, max_length=270, cache_dir=cache_dir)
+tokenizer_gpt2 = AutoTokenizer.from_pretrained("gpt2", truncate=True, max_length=270, cache_dir=cache_dir)
+
+tokenizers = {"GPTJ": tokenizer, "GPT2": tokenizer_gpt2}
 
 TOOL_START_TOKEN = "<TOOL>"
 TOOL_END_TOKEN = "</TOOL>" 
 
-tokenizer2.add_tokens([TOOL_START_TOKEN, TOOL_END_TOKEN])
+tokenizer_gpt2.add_tokens([TOOL_START_TOKEN, TOOL_END_TOKEN])
 
 
 def mask_tokenize_data(
         dataset_dir,
         output_dir,
-        tool_name:str
+        tool_name:str,
+        model_names:list[str]=["GPTJ", "GPT2"],
 ):
-    global tokenizer, TOOL_START_TOKEN, TOOL_END_TOKEN
+    global tokenizers, TOOL_START_TOKEN, TOOL_END_TOKEN
 
-    output_fields = ["tokenized_tool_text", "tokenized_tool_text2", "token_type", "token_type2", "method_A_train_mask", "method_A_train_mask2", "tool_name"]
+    # We want to output: tokenized_start_text, tool_name, tokenized_end_text, token_type, start_method_A_train_mask, end_method_A_train_mask
+    output_fields = ["tokenized_start_text", "tool_name", "tokenized_end_text", "start_token_type", "end_token_type", "start_method_A_train_mask", "end_method_A_train_mask"]
+
     # Create output directory
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    file_list = [file for file in os.listdir(dataset_dir) if file.endswith('.csv')]
+    for name in model_names:
+        if not os.path.exists(f"{output_dir}/{name}"):
+            os.makedirs(f"{output_dir}/{name}")
 
+    file_list = [file for file in os.listdir(dataset_dir) if file.endswith('.csv')]
     for file in file_list:
         dataset = load_dataset(dataset_dir, split="train", data_files = file)
         data_iter = iter(dataset)
 
-        # Create output file
-        with open(f"{output_dir}/{file}", 'w') as f:
-            writer = DictWriter(f, fieldnames=output_fields)
-            writer.writeheader()
+        # Iterate through the dataset and write to the output file
+        data = next(data_iter, None)
+        output = {}
+        for name in model_names:
+            output.update({key + name: [] for key in output_fields})
+        while data is not None:
+            
+            text = data["API_call_response_text"]
+            tokenized_text = {}
+            for model_name in model_names:
+                tokenized_text[model_name] = tokenizers[model_name](text, truncation=True, max_length=1000).input_ids
 
-            # Iterate through the dataset and write to the output file
+            # Find index where tokenized_text matches the tool start token:
+            index_start_dict = {name: tokenized_text[name].index(tokenizers[name].encode(TOOL_START_TOKEN)[0]) for name in model_names}
+            index_arrow_dict = {name: tokenized_text[name][index_start_dict[name]:].index(tokenizers[name].encode("→")[0]) + index_start_dict[name] + 1 for name in model_names}
+            index_end_dict = {name: tokenized_text[name][index_arrow[name]:].index(tokenizers[name].encode(TOOL_END_TOKEN)[0]) + index_arrow[name] + 1 for name in model_names}
+
+            len_start_dict = {name: index_start_dict[name] + 1 for name in model_names}
+
+            len_toolname_dict = {name: len(tokenizers[name].encode(tool_name)) for name in model_names}
+
+            # Find number of ocurrences of →
+            occurrences = len(re.findall(r'(\)\→)', text))
+
+            if occurrences != 1:
+                print("More than one occurrence of →", flush=True)
+                print(text, flush=True)
+                raise Exception("More than one occurrence of →")
+
+            # Create token type mask
+            token_type_dict = {name: torch.zeros(len(tokenized_text[name])) for name in model_names}
+            for name in model_names:
+                mask = token_type_dict[name]
+                len_start = len_start_dict[name]
+                len_toolname = len_toolname_dict[name]
+                index_arrow = index_arrow_dict[name]
+                index_end = index_end_dict[name]
+
+                mask[len_start] += 1                         # <TOOL>
+                mask[len_start+1] += 1                       # Toolname
+                mask[len_start+1+len_toolname] += 1        # (
+                mask[len_start+1 + len_toolname + 1] += 1    # args
+                mask[index_arrow - 1] += 1                   # )
+                mask[index_arrow] += 1                       # →
+                mask[index_arrow+1] += 1                     # response
+                mask[index_end] += 1                         # </TOOL>
+                mask[index_end+1] += 1                       # ...Data
+                mask = mask.cumsum(dim=0)
+
+                token_type_dict[name] = mask
+                output["start_method_A_train_mask" + name].append(torch.isin(token_type_dict[name][:len_start+1], torch.tensor([0, 1])).view(-1).tolist())
+                output["end_method_A_train_mask" + name].append(torch.isin(token_type_dict[name][len_start+1+len_toolname], torch.tensor([9])).view(-1).tolist())
+            
+                output["tokenized_start_text" + name].append(tokenized_text[name][:len_start+1])
+                output["tool_name" + name].append(tool_name)
+                output["tokenized_end_text" + name].append(tokenized_text[name][index_end+1:])
+                output["start_token_type" + name].append(token_type_dict[name][:len_start+1].view(-1).tolist())
+                output["end_token_type" + name].append(token_type_dict[name][len_start+1+len_toolname].view(-1).tolist())
+
+
             data = next(data_iter, None)
-            output = {}
-            while data is not None:
-                
-                text = data["API_call_response_text"]
-                tokenized_text = tokenizer(text, truncation=True, max_length=270).input_ids
-                tokenized_text2 = tokenizer2(text, truncation=True, max_length=270).input_ids
+            
 
-                # Find index where tokenized_text matches the tool start token:
-                index_start = tokenized_text.index(tokenizer.encode(TOOL_START_TOKEN)[0])
-                index_arrow = tokenized_text[index_start:].index(tokenizer.encode("→")[0]) + index_start + 1
-                index_end = tokenized_text[index_arrow:].index(tokenizer.encode(TOOL_END_TOKEN)[0]) + index_arrow + 1
 
-                len_start = index_start + 1
+        for name in model_names:
+            # Create output file
+            with open(f"{output_dir}/{name}/{file}", 'w') as f:
+                writer = DictWriter(f, fieldnames=output_fields)
+                writer.writeheader()
+                row = {key: output[key + name] for key in output_fields}
+                writer.writerow(row)
 
-                len_toolname = len(tokenizer.encode(tool_name))
-
-                # Find index where tokenized_text matches the tool start token:
-                index_start2 = tokenized_text2.index(tokenizer2.encode(TOOL_START_TOKEN)[0])
-                index_arrow2 = tokenized_text2[index_start2:].index(tokenizer2.encode("→")[0]) + index_start2 + 1
-                index_end2 = tokenized_text2[index_arrow2:].index(tokenizer2.encode(TOOL_END_TOKEN)[0]) + index_arrow2 + 1
-
-                len_start2 = index_start2 + 1
-
-                len_toolname2 = len(tokenizer2.encode(tool_name))
-
-                # Find number of ocurrences of →
-                occurrences = len(re.findall(r'(\)\→)', text))
-
-                if occurrences != 1:
-                    print("More than one occurrence of →", flush=True)
-                    print(text, flush=True)
-                    raise Exception("More than one occurrence of →")
-
-                # Create token type mask
-                token_type = torch.zeros(len(tokenized_text))      # Data...
-                token_type[len_start] += 1                         # <TOOL>
-                token_type[len_start+1] += 1                       # Toolname
-                token_type[len_start+1 + len_toolname] += 1        # (
-                token_type[len_start+1 + len_toolname + 1] += 1    # args
-                token_type[index_arrow - 1] += 1                   # )
-                token_type[index_arrow] += 1                       # →
-                token_type[index_arrow+1] += 1                     # response
-                token_type[index_end] += 1                         # </TOOL>
-                token_type[index_end+1] += 1                       # ...Data
-                token_type = token_type.cumsum(dim=0)
-
-                # Create token type mask
-                token_type2 = torch.zeros(len(tokenized_text2))      # Data...
-                token_type2[len_start2] += 1                         # <TOOL>
-                token_type2[len_start2+1] += 1                       # Toolname
-                token_type2[len_start2+1 + len_toolname2] += 1        # (
-                token_type2[len_start2+1 + len_toolname2 + 1] += 1    # args
-                token_type2[index_arrow2 - 1] += 1                   # )
-                token_type2[index_arrow2] += 1                       # →
-                token_type2[index_arrow2+1] += 1                     # response
-                token_type2[index_end2] += 1                         # </TOOL>
-                token_type2[index_end2+1] += 1                       # ...Data
-                token_type2 = token_type2.cumsum(dim=0)
-
-                method_A_train_mask = (torch.isin(token_type, torch.tensor([0, 1, 2, 9])))
-                method_A_train_mask2 = (torch.isin(token_type2, torch.tensor([0, 1, 2, 9])))
-
-                output["tokenized_tool_text"] = tokenized_text
-                output["token_type"] = token_type.tolist()
-                output["method_A_train_mask"] = method_A_train_mask.float().tolist()
-                output["tool_name"] = tool_name
-
-                output["tokenized_tool_text2"] = tokenized_text2
-                output["token_type2"] = token_type2.tolist()
-                output["method_A_train_mask2"] = method_A_train_mask2.float().tolist()
-
-                writer.writerow(output)
-
-                data = next(data_iter, None)
 
 
 if __name__ == "__main__":
