a21116a6dc7fa35bec35d3e58da5de53a70848dc
j <jg2619@ic.ac.uk>
2023-08-14 22:12:55 +0100

    gitignore llama layers

--- model_training/model_experiments/model_layers.py
+++ model_training/model_experiments/model_layers.py
@@ -3,17 +3,27 @@
 
 import torch
 
-"""
-from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from transformers import LlamaForCausalLM
 
 cache_dir = "/vol/bitbucket/jg2619/augmenting_llms/augmented_data_pipeline/toolformer/cache"
-model = AutoModelForCausalLM.from_pretrained(
+"""model = AutoModelForCausalLM.from_pretrained(
             "EleutherAI/gpt-j-6B",
             revision="float16",
             torch_dtype=torch.float16,
             low_cpu_mem_usage=True,          # CANT HANDLE DEEPSPEED ZERO 3
             cache_dir=cache_dir 
-        )
+        )"""
+
+
+
+model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf",
+                                            torch_dtype=torch.float16,
+                                            low_cpu_mem_usage=True,
+                                            token="***REMOVED***",
+                                            cache_dir=cache_dir)
+
+
 
 print("Model named parameters")
 layer_names = [name for name, p in model.named_parameters()]
@@ -21,6 +31,6 @@ print(layer_names)
 for name, p in model.named_parameters():
     print(name)
     print(p.shape)
-"""
+
 
 print(torch.cuda.get_device_properties(0))
