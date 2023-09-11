2419d68a4fa0aa7bded95718236ba8f4a5f63489
jg2619 <jg2619@ic.ac.uk>
2023-09-11 05:52:23 +0100

    small changes

--- model_training/models/tokenizer/prepare_tokenizer.py
+++ model_training/models/tokenizer/prepare_tokenizer.py
@@ -1,13 +1,16 @@
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, LlamaTokenizer
 
 cache_dir = "/vol/bitbucket/jg2619/augmenting_llms/augmented_data_pipeline/toolformer/cache"
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B", cache_dir=cache_dir)
+# tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B", cache_dir=cache_dir)
+tokenizer = LlamaTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf",
+                                                   token="***REMOVED***",
+                                                   cache_dir=cache_dir)
 
 TOOL_START_TOKEN = "<TOOL>"
 TOOL_END_TOKEN = "</TOOL>" 
 
 tokenizer.add_tokens([TOOL_START_TOKEN, TOOL_END_TOKEN])
-tokenizer.add_special_tokens({"pad_token":"[PAD]"})
-tokenizer.pad_token = "[PAD]"
+#tokenizer.add_special_tokens({"pad_token":"[PAD]"})
+#tokenizer.pad_token = "[PAD]"
 
-tokenizer.save_pretrained("/vol/bitbucket/jg2619/augmenting_llms/model_training/models/tokenizer/")
+tokenizer.save_pretrained("/vol/bitbucket/jg2619/augmenting_llms/model_training/models/tokenizer-llama/")
